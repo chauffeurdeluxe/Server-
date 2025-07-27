bc38ab8de0cc3a516e6bb980a0bc2f18707d70b1
chauffeurdeluxe <chauffeurdeluxe@yahoo.com>
2025-07-27 10:55:45 +1000

    Update server.js

--- server.js
+++ server.js
@@ -11,7 +11,7 @@ app.post('/create-checkout-session', async (req, res) => {
 
   try {
     const session = await stripe.checkout.sessions.create({
-      payment_method_types: ['card'],
+      payment_method_collection: 'always',
       line_items: [{
         price_data: {
           currency: 'aud',
