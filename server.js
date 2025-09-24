aad223161f2baa97a6e13ae56eef184dff8883c4
chauffeurdeluxe <chauffeurdeluxe@yahoo.com>
2025-09-24 22:53:27 +1000

    Update server.js

--- server.js
+++ server.js
@@ -1,4 +1,3 @@
-require('dotenv').config();
 console.log("DEBUG: SUPABASE_URL =", `"${process.env.SUPABASE_URL}"`);
 console.log("DEBUG: SUPABASE_SERVICE_ROLE_KEY =", `"${process.env.SUPABASE_SERVICE_ROLE_KEY}"`);
 const express = require('express');
